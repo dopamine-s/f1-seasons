eef9d89f3b333666b1f6a6f8b3deca4527c71e94
Dopamine <motor55005@gmail.com>
2022-11-29 18:25:14 +0100

    CSS and Grid improvement

--- src/components/Round/RoundResult.js
+++ src/components/Round/RoundResult.js
@@ -24,7 +24,7 @@ const RoundResult = (props) => {
         <ResultInfoItem title={'Team'} info={props.team} />
         <ResultInfoItem title={'Time'} info={props.time} />
         <FavoriteAddButton
-          info={'Add Fav Pilot'}
+          info={'+'}
           // onAdd={props.onAdd}
           // driverId={props.driverId}
         />
