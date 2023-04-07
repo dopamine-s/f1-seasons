efa226d45e76bbec0ebf6ed5b4076186efe70177
dopamine-s <motor55005@gmail.com>
2023-04-07 11:52:24 +0200

    current year small fix

--- src/components/Seasons/SeasonSelect.js
+++ src/components/Seasons/SeasonSelect.js
@@ -1,30 +1,30 @@
 import classes from './SeasonSelect.module.css';
 
 const SeasonSelect = (props) => {
-  const changeSeasonYearHandler = (event) => {
-    const seasonYear = event.target.value;
+	const changeSeasonYearHandler = (event) => {
+		const seasonYear = event.target.value;
 
-    props.onChangeSeason(seasonYear);
-  };
+		props.onChangeSeason(seasonYear);
+	};
 
-  const reversed = props.seasons.sort((a, b) => b.season - a.season);
+	const reversed = props.seasons.sort((a, b) => b.season - a.season);
 
-  return (
-    <div className={classes['season-select']}>
-      <div className={classes.control}>
-        <label>Select season:</label>
-        <select value={props.selected} onChange={changeSeasonYearHandler}>
-          {props.seasons.length > 0 ? (
-            reversed.map((season) => (
-              <option key={season.season}>{season.season}</option>
-            ))
-          ) : (
-            <option key={'2022'}>{props.selected}</option>
-          )}
-        </select>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classes['season-select']}>
+			<div className={classes.control}>
+				<label>Select season:</label>
+				<select value={props.selected} onChange={changeSeasonYearHandler}>
+					{props.seasons.length > 0 ? (
+						reversed.map((season) => (
+							<option key={season.season}>{season.season}</option>
+						))
+					) : (
+						<option key={props.selected}>{props.selected}</option>
+					)}
+				</select>
+			</div>
+		</div>
+	);
 };
 
 export default SeasonSelect;
